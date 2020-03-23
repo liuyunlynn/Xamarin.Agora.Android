fc9bc922d006c18ac3eb6b22a9ffe553d7de8ff3
Yun <yun.liu@feisystems.com>
2020-03-23 20:26:05 +0800

    Add signarlR

--- AgoraVideoCall/MainActivity.cs
+++ AgoraVideoCall/MainActivity.cs
@@ -9,35 +9,71 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using Xamarin.Essentials;
-using Toolbar = Android.Support.V7.Widget.Toolbar;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace AgoraVideoCall
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            //var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            //SetSupportActionBar(toolbar);
-
-            //var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            //fab.Click += FabOnClick;
-
-            var btnOne = FindViewById<Button>(Resource.Id.button_user1);
-            btnOne.Click += BtnOne_Click;
+            var login = FindViewById<Button>(Resource.Id.login);
+            login.Click += Login_Click;
+            var call = FindViewById<Button>(Resource.Id.call);
+            call.Click += Call_Click;
+           
         }
 
-        private void BtnOne_Click(object sender, EventArgs e)
+        private async void Call_Click(object sender, EventArgs e)
         {
+            var callId= FindViewById<EditText>(Resource.Id.callId).Text;
+            var client = SignalRHubClient.Instance;
+            await client.Send(callId, "Video Call");
             var intent = new Intent(this, typeof(VideoCallActivity));
-
             StartActivity(intent);
         }
+
+        private async void Login_Click(object sender, EventArgs e)
+        {
+            var client = SignalRHubClient.Instance;
+            try
+            {
+                var userId = FindViewById<EditText>(Resource.Id.userid).Text;
+                await client.Connect(userId);
+                client.OnReceiveEvent += (id, message) =>
+                {
+                    RunOnUiThread(() =>
+                    {
+                        var content = message + id;
+                        var alertDialog = new AlertDialog.Builder(this);
+                        alertDialog.SetTitle("Video Call").SetMessage(content);
+                        alertDialog.SetCancelable(true);
+                        alertDialog.SetNegativeButton("Denial",
+                            (s, d) =>
+                            {
+
+                            });
+                        alertDialog.SetPositiveButton("Answer",
+                            (s, d) =>
+                            {
+                                var intent = new Intent(this, typeof(VideoCallActivity));
+                                StartActivity(intent);
+                            });
+                        alertDialog.Show();
+                    });
+                  
+                };
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
---
+++ AgoraVideoCall/SignalRHubClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace AgoraVideoCall
+{
+    public class SignalRHubClient
+    {
+        private static SignalRHubClient _instance;
+
+        private readonly HubConnection _connection;
+        private readonly IHubProxy _proxy;
+
+        public SignalRHubClient(string serverUrl, string hubName)
+        {
+            _connection = new HubConnection(serverUrl);
+            _proxy = _connection.CreateHubProxy(hubName);
+        }
+
+        public static SignalRHubClient Instance =>
+            _instance ??= new SignalRHubClient("http://192.168.1.2:59062/", "ServerHub");
+
+        public event EventHandler<string> OnReceiveEvent;
+
+        public async Task Connect(string id)
+        {
+            await _connection.Start();
+            var work = _proxy.Invoke("Register", id);
+            _proxy.On("SendMessage", (string userId, string content) => { OnReceiveEvent?.Invoke(userId, content); });
+        }
+
+        public Task Send(string username, string message)
+        {
+            var serverMethod = "Send";
+            if (_connection.State != ConnectionState.Connected) return null;
+
+            return _proxy.Invoke(serverMethod, username, message);
+        }
+    }
+}
